9e8313aee6d551395d2d64cd499c195dca374067
SvenZ <zbinden.sven@gmail.com>
2023-07-05 19:49:42 +0200

    added Models Vehicle and VehicleImage

--- vehicleImage/models.py
+++ vehicleImage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from vehicle.models import Vehicle
+
+
 # Create your models here.
+class VehicleImage(models.Model):
+    image = models.ImageField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    vehicle = models.ForeignKey(to=Vehicle, on_delete=models.CASCADE, null=True)
