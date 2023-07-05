e26fd3777aa3776c0d773adcca1355f1f5699b8b
SvenZ <zbinden.sven@gmail.com>
2023-07-05 21:02:03 +0200

    added directory_path for vehicleImage

--- vehicleImage/models.py
+++ vehicleImage/models.py
@@ -3,8 +3,14 @@ from django.db import models
 from vehicle.models import Vehicle
 
 
+def vehicle_image_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    print(instance)
+    return f'{instance.id}/{filename}'
+
+
 # Create your models here.
 class VehicleImage(models.Model):
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to=vehicle_image_directory_path, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     vehicle = models.ForeignKey(to=Vehicle, on_delete=models.CASCADE, null=True)
