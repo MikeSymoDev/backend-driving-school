bea47925e3d620b8b4c0cbea0fce60802e86a733
SvenZ <zbinden.sven@gmail.com>
2023-07-06 20:54:18 +0200

    used djangoSignals to create UserProfile

--- userProfile/models.py
+++ userProfile/models.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from drivingSchool.models import DrivingSchool
 from user.models import User
@@ -68,7 +70,15 @@ class UserProfile(models.Model):
             raise ValidationError({'driving_school': 'Driving School is required for instructors.'})
 
     def __str__(self):
-        user_type = dict(self.TYPE)[self.type]
-        return f'{self.email} ({user_type})'
+        # user_type = dict(self.TYPE)[self.type]
+        return f'{self.email}'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, email=instance.email)
 
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.user.save()
