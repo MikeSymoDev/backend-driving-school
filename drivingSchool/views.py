53f068a8e45e2c366547af08609abdfc39110d16
SvenZ <zbinden.sven@gmail.com>
2023-07-08 11:21:07 +0200

    check master

--- drivingSchool/views.py
+++ drivingSchool/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+
+from drivingSchool.models import DrivingSchool
+from drivingSchool.serializers import DrivingSchoolSerializer
+
 
 # Create your views here.
+class ListCreateDrivingSchoolView(ListCreateAPIView):
+    queryset = DrivingSchool.objects.all()
+    serializer_class = DrivingSchoolSerializer
