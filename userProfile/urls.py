fa615418478152411cb9f88f2b5b382d8f966a8c
SvenZ <zbinden.sven@gmail.com>
2023-07-08 09:29:57 +0200

    improved retrieve, update, delete view, added get instructors view

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from userProfile.views import RetrieveUpdateDeleteUserProfile, SetupUserProfileView
+from userProfile.views import RetrieveUpdateDeleteUserProfile, SetupUserProfileView, InstructorListView
 
 urlpatterns = [
     path('setup/', SetupUserProfileView.as_view()),
-    path('edit/<int:userProfile_id>', RetrieveUpdateDeleteUserProfile.as_view())
+    path('me/edit/', RetrieveUpdateDeleteUserProfile.as_view()),
+    path('instructors/', InstructorListView.as_view())
 ]
