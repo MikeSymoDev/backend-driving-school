24b19fd2a287396d8c3ad9315923339260478cf0
SvenZ <zbinden.sven@gmail.com>
2023-07-12 22:07:18 +0200

    student sees own bookings

--- appointment/urls.py
+++ appointment/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from appointment.views import AppointmentCreateAPIView, AppointmentRetrieveUpdateDeleteView, AppointmentByDateView, \
-    SetAppointmentsUnavailableAPIView, SetSingleAppointmentUnavailableView
+    SetAppointmentsUnavailableAPIView, SetSingleAppointmentUnavailableView, StudentAppointmentsView
 
 urlpatterns = [
     path('', AppointmentCreateAPIView.as_view(), name='booking-list'),
     path('not-available/', SetAppointmentsUnavailableAPIView.as_view(), name='not-available-many'),
     path('not-available/<int:pk>', SetSingleAppointmentUnavailableView.as_view(), name='not-available-single'),
     path('<int:pk>', AppointmentRetrieveUpdateDeleteView.as_view(), name='booking-detail'),
-    path('by-date/<int:instructor_id>/', AppointmentByDateView.as_view())
+    path('by-date/<int:instructor_id>/', AppointmentByDateView.as_view()),
+    path('mybookings/', StudentAppointmentsView.as_view())
 ]
